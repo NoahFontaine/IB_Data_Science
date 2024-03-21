853ee49f05b9261eb3e328c5c02c9b8f304179a4
Noah Fontaine <nf391@cam.ac.uk>
2024-03-21 18:02:27 +0000

    Finished Ex1

--- Exercises/.ipynb_checkpoints/Exercise 1 checkpoint.py
+++ Exercises/.ipynb_checkpoints/Exercise 1 checkpoint.py
@@ -68,3 +68,81 @@ def polyreg(data_matrix, k):
     return(beta_vec, SSE, resid_vec)
 
 polyreg(xy_data, 7)
+
+
+"""
+Exercise 1c) Use `polyreg` to fit polynomial models for the data in `xy_data` for $k=2,3,4$:
+"""
+
+# Points of the data
+x = xy_data[:,0]  # First column of array (indexed by 0)
+y = xy_data[:,1]  # Second column of array (indexed by 1)
+
+# Plot of the poly model for k = 2, with the same x_coordinates as data
+beta_vec_k2 = polyreg(xy_data, 2)[0]
+y_k2 = beta_vec_k2[0] + beta_vec_k2[1]*x + beta_vec_k2[2]*x**2
+
+# Poly model for k = 3
+beta_vec_k3 = polyreg(xy_data, 3)[0]
+y_k3 = beta_vec_k3[0] + beta_vec_k3[1]*x + beta_vec_k3[2]*x**2 + beta_vec_k3[3]*x**3
+
+# Poly model for k = 4
+beta_vec_k4 = polyreg(xy_data, 4)[0]
+y_k4 = beta_vec_k4[0] + beta_vec_k4[1]*x + beta_vec_k4[2]*x**2 + beta_vec_k4[3]*x**3 + beta_vec_k4[4]*x**4
+
+# SSE for the models and SSE_0
+SSE_0 = polyreg(xy_data, 0)[1]
+SSE_2 = polyreg(xy_data, 2)[1]
+SSE_3 = polyreg(xy_data, 3)[1]
+SSE_4 = polyreg(xy_data, 4)[1]
+
+print("The SSEs for the polynomial models with k = (2,3,4) are (" + str(np.round(SSE_2, 3)) + ", " + str(np.round(SSE_3, 3)) + ", " + str(np.round(SSE_4, 3)) + ") respectively.")
+
+# Calculate R^2 values
+R_2 = np.round(1-SSE_2/SSE_0, 3)
+R_3 = np.round(1-SSE_3/SSE_0, 3)
+R_4 = np.round(1-SSE_4/SSE_0, 3)
+
+print("The R^2 values for the polynomial models with k = (2,3,4) are (" + str(R_2) + ", " + str(R_3) + ", " + str(R_4) + ") respectively.")
+
+plt.scatter(x, y, s=10)
+plt.plot(x, y_k2, "-", label = "$k = 2$")
+plt.plot(x, y_k3, "-", label = "$k = 3$")
+plt.plot(x, y_k4, "-", label = "$k = 4$")
+plt.xlabel('x values')
+plt.ylabel('y values')
+plt.title('Plot of the scatterplot with the polynomial models for $k = n$')
+plt.legend()
+plt.savefig('Ex1c_plot.pdf', bbox_inches = 'tight')
+plt.show()
+
+
+"""
+Exercise 1d) For the model you have chosen in the previous part (either $k=2/3/4)$:
+"""
+
+# Scatter plot of the residuals
+plt.rcParams['figure.figsize'] = [10, 5]
+plt.scatter(x, polyreg(xy_data, 3)[2])
+plt.title("Scatter plot of the residuals for $k=3$")
+plt.xlabel("x-values")
+plt.ylabel("Residuals")
+plt.show()
+
+# Histogram of the residuals with the Gaussian pdf
+print('Mean of residuals = ', np.round(np.mean(polyreg(xy_data, 3)[2]), 5), 'Variance of residuals = ', 
+      np.round(np.var(polyreg(xy_data, 3)[2]), 3))
+
+# Plot normed histogram of the residuals
+n, bins, patches = plt.hist(polyreg(xy_data, 3)[2], bins=14, density=True, facecolor='green');
+
+# Plot Gaussian pdf with same mean and variance as the residuals
+from scipy.stats import norm
+
+res_stdev = np.std(polyreg(xy_data, 3)[2])  #standard deviation of residuals
+xvals = np.linspace(-3*res_stdev,3*res_stdev,1000)
+plt.plot(xvals, norm.pdf(xvals, loc=0, scale=res_stdev), 'r')
+plt.xlabel("Residuals")
+plt.ylabel("Relative frequency")
+plt.title("Histogram of the residuals for $k=3$ with its Gaussian pdf")
+plt.show()
